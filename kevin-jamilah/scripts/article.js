260a95133bf4059fd49a07893dfc938f0b8decb7
Kevinoh47 <kohok47@gmail.com>
2018-07-28 13:51:27 -0700

    added our css and first set of updates to code

--- kevin-jamilah/scripts/article.js
+++ kevin-jamilah/scripts/article.js
@@ -2,23 +2,32 @@
 
 let articles = [];
 
-// COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
+// COMMENTED: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
+// This is a constructor function; by convention constructor functions in js are capitalized.
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object that is passed in to complete this constructor function:
+  // TODONE: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
-  this.property = rawDataObj.property;
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.category;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
 }
 
 Article.prototype.toHtml = function() {
-  // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // PUT YOUR RESPONSE HERE
+  // COMMENTED: What is the benefit of cloning the article? (see the jQuery docs)
+  // "When used in conjunction with one of the insertion methods, .clone() is a convenient way to duplicate elements on a page" -- https://api.jquery.com/clone/
 
   let $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  /* TODONE: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  $newArticle.removeClass('template');
 
-  if (!this.publishedOn) $newArticle.addClass('draft');
+  if (!this.publishedOn) {
+    $newArticle.addClass('draft');
+  }
+
   $newArticle.attr('data-category', this.category);
 
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
@@ -51,7 +60,7 @@ for(let i = 0; i < articles.length; i++) {
   // REVIEW: below code will hang until TODO about cloned article is handled
   // Once that TODO is done uncomment code
 
-  // $('#articles').append(articles[i].toHtml());
+  $('#articles').append(articles[i].toHtml());
 
   // COMMENT: (STRETCH) Can you figure out why code hangs?
   // It has to do with the clone() method
